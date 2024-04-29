d520a9cc504ab2939a334ee31d16ad5dec30b888
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-29 19:18:05 +0100

    project change

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -16,7 +16,7 @@ export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
   const pathname = usePathname();
   return (
     <div className="link relative">
-      <Link {...props} />;
+      <Link {...props} />
       <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-primary transition-all duration-300"></span>
     </div>
   );
