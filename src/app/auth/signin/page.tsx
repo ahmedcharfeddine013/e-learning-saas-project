64b5b30c37fc70fe0e41cbf44f5e23bb8933afc0
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-30 11:08:45 +0100

    unauthorized

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+import {  getProviders, signIn } from "next-auth/react";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { useRouter } from "next/router";
 
-export default function page() {
+export default function SignInPage({
+  providers,
+  session,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
+
+  // If the user is already authenticated, redirect to the home page
+  if (session) {
+    router.replace("/");
+    return null; // Return null to avoid rendering the sign-in page
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  // If the user is already logged in, redirect.
+
+  if (session) {
+    return { redirect: { destination: "/" } };
+  }
+
+  const providers = await getProviders();
+
+  return {
+    props: { providers: providers ?? [], session },
+  };
 }
