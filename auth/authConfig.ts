a0e2b5c778428d41ee360e41f4fc83f182806536
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-04-30 12:08:55 +0100

    barcha bugs

---
+++ auth/authConfig.ts
@@ -0,0 +1,40 @@
+import type { NextAuthConfig } from "next-auth"
+import Credentials from 'next-auth/providers/credentials'
+import { LoginSchema } from "@/schemas/schema";
+import { getUserByEmail } from "../data/user";
+import bcrypt from 'bcryptjs';
+import GithubProvider from 'next-auth/providers/github'
+import  Providers  from "next-auth/providers";
+import GoogleProvider from "next-auth/providers/google"
+
+export default {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+    GithubProvider({
+      clientId : process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    }),
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials)
+
+        if(validatedFields.success) {
+          const {email, password} = validatedFields.data
+          const user = await getUserByEmail(email) 
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare (
+            password, 
+            user.password,
+          )
+
+          if (passwordsMatch) return user
+        }
+        return null
+      }
+    })
+  ],
+} satisfies NextAuthConfig
--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,24 +1,54 @@
-import React from "react";
+"use client";
+
+import React, { useState, useTransition } from "react";
 import { LoginSchema } from "@/schemas/schema";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from 'zod'
+import * as z from "zod";
 import { Card } from "../ui/card";
 import { Form } from "../ui/form";
 
 export default function LoginForm() {
-  const form = useForm<z.infer <typeof LoginSchema>>({
+  const [showTwoFactor, setShowTwoFactor] = useState(false);
+
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
       email: "",
       password: "",
     },
   });
-  return <Card>
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(() => {})} >
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    setError("");
+    setSuccess("");
+    startTransition(() => {
+      login(values)
+        .then((data) => {
+          if (data?.error) {
+            form.reset();
+            setError(data.error);
+          }
+          if (data?.success) {
+            form.reset();
+            setSuccess(data.success);
+          }
+          if (data?.twoFactor) {
+            setShowTwoFactor(true);
+          }
+        })
+        .catch(() => setError("Something went wrong!"));
+    });
+  };
 
-      </form>
-    </Form>
-  </Card>;
+  return (
+    <Card>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})}></form>
+      </Form>
+    </Card>
+  );
 }
